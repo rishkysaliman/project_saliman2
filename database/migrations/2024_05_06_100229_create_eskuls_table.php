20469848c5c4f46267d5db3a3bd3a941b3ae4a6f
rishkysaliman <161911154+rishkysaliman@users.noreply.github.com>
2024-05-06 17:16:31 +0700

    Add files via upload

---
+++ database/migrations/2024_05_06_100229_create_eskuls_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eskuls', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_siswa');
+            $table->string('bola');
+            $table->string('basket');
+            $table->string('voli');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eskuls');
+    }
+};
