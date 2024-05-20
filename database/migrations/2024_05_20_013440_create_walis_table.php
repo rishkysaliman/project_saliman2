f681548addc90ad9bc1437bd496ddfcd10591198
rishkysaliman <161911154+rishkysaliman@users.noreply.github.com>
2024-05-20 14:55:21 +0700

    Add files via upload

---
+++ database/migrations/2024_05_20_013440_create_walis_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('walis', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->bigInteger('id_mahasiswa')->unsigned();
+            $table->foreign('id_mahasiswa')->references('id')
+                  ->on('mahasiswas')->ondelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('walis');
+    }
+};
